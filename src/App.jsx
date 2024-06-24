ace923825dc042feaa9b9d7439fbb477ec423645
calvinyueWillTry <calvin.yue@gmail.com>
2024-06-24 11:14:21 -0700

    tutor additions

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./application.css";
+import portfolio from "./components/portfolio";
+//react is architect, like lego blocks
+function App() {
+    return (<div className="portfolio_App"></div>)
+}
+
+//all files needs to be reusable
+//website = book with chapters/pages, webpage has components to create one website
